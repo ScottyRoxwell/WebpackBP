91acbf8bd72fc328024fec4f3c4e5a9b50c05b97
ScottyRoxwell <scottyroxwell@gmail.com>
2019-10-04 10:49:52 -0700

    Better spiral

--- src/scripts/myThree.js
+++ src/scripts/myThree.js
@@ -1,8 +1,7 @@
 import {THREE} from '../vendor';
-import { Cylindrical } from 'three';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(50,window.innerWidth/window.innerHeight,1,1000);
+const camera = new THREE.PerspectiveCamera(90,window.innerWidth/window.innerHeight,1,12000);
 
 var light = new THREE.PointLight( 0xffffff, 100, 500);
 scene.add( light );
@@ -22,60 +21,84 @@ canvas.appendChild(renderer.domElement);
 // cylinder.rotation.x = (Math.PI)/2;
 // scene.add( cylinder );
 
-class Point{
-  constructor(radius,theta,y){
-    this.radius = radius;
-    this.theta = theta;
-    this.y = y;
-    // let cylinderPoint = new Cylindrical(this.radius,this.theta,this.y);
-    this.x = this.radius * Math.cos(this.theta);
-    this.y = y;
-    this.z = this.radius * Math.sin(this.theta);
-    // this.cylPoint = new THREE.Cylindrical(this.radius,this.theta,this.y);
+// class Point{
+//   constructor(radius,theta,y){
+//     this.radius = radius;
+//     this.theta = theta;
+//     this.y = y;
+//     // let cylinderPoint = new Cylindrical(this.radius,this.theta,this.y);
+//     this.x = this.radius * Math.cos(this.theta);
+//     this.y = y;
+//     this.z = this.radius * Math.sin(this.theta);
+//     // this.cylPoint = new THREE.Cylindrical(this.radius,this.theta,this.y);
     
     
 
-  }
-}
+//   }
+// }
 
 
 class Ball{
-  constructor(_x=0,_y=0,_z=0){
-    this.x = _x;
-    this.y = _y;
-    this.z = _z;
-    this.geometry = new THREE.SphereGeometry(2,22,22);
-    this.material = new THREE.MeshPhysicalMaterial({color:0xffffff});
+  constructor(radius,theta,y){
+    this.radius = radius;
+    this.theta = theta;
+    this.x = this.radius * Math.cos(this.theta);
+    this.y = y;
+    this.z = this.radius * Math.sin(this.theta);
+    this.life = 0;
+    this.geometry = new THREE.SphereGeometry(2,3,3);
+    this.material = new THREE.MeshBasicMaterial({color:0xffffff});
     this.sphere = new THREE.Mesh(this.geometry,this.material);
     this.sphere.position.x = this.x;
     this.sphere.position.y = this.y;
     this.sphere.position.z = this.z;
+    this.speed = Math.random()*.018;
+  }
+  move(){
+    this.theta -= this.speed;
+    this.sphere.position.x = this.radius * Math.cos(this.theta);
+    this.sphere.position.z = this.radius * Math.sin(this.theta);
   }
 }
 
 let theta = 0;
-let i = 0;
 
-while(i < 2000){
-  i += 20;
+let balls = [];
+
+let i = 0;
+while(balls.length < 6000){
+  i +=1;
 
   theta = (Math.random())*10;
-  let point = new Point(150,theta,i);
-  console.log(point);
-  let ball = new Ball(point.x,point.y,point.z);
-  scene.add(ball.sphere);
-}
-console.log(scene)
+  let ball = new Ball(250,theta,i);
+  ball.life = 1200;
+  balls.push(ball);
 
-camera.position.y = -40;
-camera.lookAt(0,400,0);
+  scene.add(ball.sphere)
+}
+
+camera.position.y = -380;
+camera.lookAt(0,20,0);
 
 // console.log(direction)
 
 
 var animate = function () {
   requestAnimationFrame( animate );
-  camera.position.y += .02;
+  camera.position.y += 7;
+  balls.forEach(ball => {
+    if(ball.life > 0){
+      ball.life--;
+    } else {
+      balls.pop();
+    }
+    
+    ball.move();
+    
+  })
+  
+  
+  // ball.life--;
 	// cylinder.rotation.x += 0.01;
   // cylinder.rotation.y += 0.01;
   // cylinder.rotation.z += 0.0001;
