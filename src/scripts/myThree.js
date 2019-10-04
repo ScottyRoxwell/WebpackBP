a5872f21cda86d34f80c24e36e458aab602ffb2b
ScottyRoxwell <scottyroxwell@gmail.com>
2019-10-03 20:56:53 -0700

    The beginnings of flying through the cylinder added to myThree.js

--- src/scripts/myThree.js
+++ src/scripts/myThree.js
@@ -1,9 +1,9 @@
 import {THREE} from '../vendor'
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,.1,1000);
+const camera = new THREE.PerspectiveCamera(30,window.innerWidth/window.innerHeight,1,20);
 
-var light = new THREE.AmbientLight( 0xffffff ); // soft white light
+var light = new THREE.PointLight( 0xffffff, 0.01, 100 );
 scene.add( light );
 
 var renderer = new THREE.WebGLRenderer();
@@ -12,18 +12,24 @@ const canvas = document.querySelector('.canvas');
 canvas.appendChild(renderer.domElement);
 
 var geometry = new THREE.CylinderGeometry( 5, 5, 40, 40, 64, true );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var material = new THREE.MeshPhysicalMaterial( { color: 0x00ff00 } );
+material.side = THREE.DoubleSide;
 var cylinder = new THREE.Mesh( geometry, material );
+cylinder.rotation.x = (Math.PI)/2;
 scene.add( cylinder );
 
 
 camera.position.z = 40;
 
+// console.log(direction)
+
+
 var animate = function () {
-	requestAnimationFrame( animate );
-	cylinder.rotation.x += 0.01;
-  cylinder.rotation.y += 0.01;
-  cylinder.rotation.z += 0.0001;
+  requestAnimationFrame( animate );
+  camera.position.z -= .002;
+	// cylinder.rotation.x += 0.01;
+  // cylinder.rotation.y += 0.01;
+  // cylinder.rotation.z += 0.0001;
 	renderer.render( scene, camera );
 };
 
