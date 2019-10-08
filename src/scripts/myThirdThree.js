ac88bc602b990247f97e0ecd1576804487da43e0
ScottyRoxwell <scottyroxwell@gmail.com>
2019-10-08 00:31:53 -0700

    Slow going but got first ring spinning with respective randomThetas.

--- src/scripts/myThirdThree.js
+++ src/scripts/myThirdThree.js
@@ -13,8 +13,10 @@ renderer.setSize(window.innerWidth,window.innerHeight);
 const canvas = document.querySelector('.canvas');
 canvas.appendChild(renderer.domElement);
 
-camera.position.y = -160;
+camera.position.y = -90;
 camera.lookAt(0,20,0);
+
+let planes = [];
 
 class Plane{
   constructor(radius,theta,y,size){
@@ -24,49 +26,62 @@ class Plane{
     this._size = size;
     this._x = this._radius * Math.cos(this._theta);
     this._z = this._radius * Math.sin(this._theta);
+    this._randomTheta = 0;
   }
-  draw(){
+  draw(rT){
     let geometry = new THREE.PlaneGeometry(this._size,this._size);
     let material = new THREE.MeshBasicMaterial({color: 0x00ff00, wireframe: false, side: THREE.DoubleSide});
     let plane = new THREE.Mesh(geometry,material);
     plane.position.x = this._x;
     plane.position.z = this._z;
-    // plane.rotateX(Math.PI/2)
-    plane.lookAt(0,plane.position.y,0);
+    plane._randomTheta = rT;
+    planes.push(plane);
     console.log(plane)
     scene.add(plane);
   }
 }
 
 class Wheel{
-  constructor(y, radius, size) {
+  constructor(y, radius, size, speed) {
     this._y = y;
     this._radius = radius;
     this._size = size;
+    this._speed = speed;
   }
   makeWheel(){
     for(let i = 0; i < Math.random()*60; i++){
-      let plane = new Plane(this._radius,(180/Math.PI)*Math.random()*Math.PI*2,this._y,this._size);
-      plane.draw();
-      
-      
+      let randomTheta = Math.random()*Math.PI*2;
+      let plane = new Plane(this._radius,randomTheta,this._y,this._size);
+      plane.draw(randomTheta);  
     }
+  }
+  spin(){
+    theta -= this._speed;
+    planes.forEach(plane => {
+      plane.position.x = this._radius * Math.cos(theta + plane._randomTheta);
+      plane.position.z = this._radius * Math.sin(theta + plane._randomTheta);
+      plane.lookAt(0,plane.position.y,0);
+    
+    })
+    
     
   }
+
 }
 
-let wheel = new Wheel(20,40,4)
-console.log(wheel)
+let wheel = new Wheel(40,20,2, 0.02)
 wheel.makeWheel()
-console.log(scene)
 
-
-// var animate = function () {
-//   requestAnimationFrame( animate );
-
+let theta = 0;
+const animate = function () {
+  requestAnimationFrame( animate );
+  wheel.spin()
+  planes.forEach(plane =>{
+    console.log(plane.position.x,plane.position.y,plane.position.z)
+  })
 
 
    renderer.render( scene, camera );
-// };
+};
 
-// animate();
+animate();
