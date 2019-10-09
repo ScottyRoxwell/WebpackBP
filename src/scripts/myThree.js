310b3e708a9326627ea5749108b94e18761ffb39
ScottyRoxwell <scottyroxwell@gmail.com>
2019-10-08 18:17:27 -0700

    The tunnel is starting to look pro!

--- src/scripts/myThree.js
+++ src/scripts/myThree.js
@@ -2,14 +2,16 @@ import {THREE} from '../vendor';
 import { identifier, isInterfaceDeclaration } from 'babel-types';
 import { transcode } from 'buffer';
 
+// Variables
 let distance = 5000;
 let descentSpeed;
-let tunnelEnd = distance;
+let density = 55;
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(78,window.innerWidth/window.innerHeight,1,distance);
 
-init(distance, -80, 2.98);
+// Initiate Program
+init(distance, -80, .98);
 function init(depth,camStart,descent){
   distance = depth;
   descentSpeed = descent;
@@ -35,7 +37,7 @@ const canvas = document.querySelector('.canvas');
 canvas.appendChild(renderer.domElement);
 
 
-
+// Plane Class
 class Plane{
   constructor(radius,theta,y){
     this.radius = radius;
@@ -52,8 +54,9 @@ class Plane{
     this.mesh.position.z = this.z;
     let r = Math.random();
     this.gear = -Math.random()*.032;
-    this.speed = r < .97 ? this.speed = this.gear*.1: this.gear;
+    this.speed = r < .97 ? this.speed = this.gear*.1 : this.gear;
   }
+  //function to spin the planes
   move(){
     this.theta += this.speed;
     this.mesh.position.x = this.radius * Math.cos(this.theta);
@@ -69,7 +72,7 @@ while(i < distance){
   theta = Math.random()*10;
   let plane = new Plane(650,theta,i);
   elements.push(plane);
-  let randoNum = Math.random()*55;
+  let randoNum = Math.random()*density;
   for(let k = 0; k < randoNum; k++){
     let setSpeed = plane.speed;
     theta = Math.random()*10;
@@ -83,14 +86,14 @@ while(i < distance){
   // Space allotted for ring
   i+=52;
 }
-
+let delta = 0;
 var animate = function () {
   requestAnimationFrame( animate );
   
   // ANIMATE LIGHTS
   let random = Math.random();
 
-  (random > .97) ? ambient.intensity += .9 : ambient.intensity -= (ambient.intensity > .3) ? .04 : 0;
+  (random > .97) ? ambient.intensity += .9 : ambient.intensity -= (ambient.intensity > .4) ? .04 : 0;
 
   if(random > .95){
     light.position.y = Math.random()*(camera.position.y + distance);
@@ -101,7 +104,24 @@ var animate = function () {
   (random > .2) ? light.position.y += 1050 : light.position.y -= 250;
 
   // Move camera up Y-Axis
-  camera.position.y += descentSpeed;
+  camera.position.y += descentSpeed;;
+
+  // Camera Movement
+  ++delta;
+  camera.position.x = Math.cos(delta*.01234)*200;
+  camera.position.z = Math.sin(delta*.01234)*80;
+  camera.rotation.z += .003;
+  // random > .3 ? camera.rotation.z += .003 : camera.rotation.z -=.003;
+
+  let cameraSpeed = (random > .9) ? 8 : 1;
+  camera.position.y += cameraSpeed;
+  if(cameraSpeed > .005){
+    cameraSpeed -= .0008;
+  }
+  if(camera.position.y > cameraSpeed)
+  camera.position.y -= .009;
+  
+  //move on the z axis
 
   function recyclePlanes(arr){
     arr.forEach(plane => {
@@ -114,7 +134,6 @@ var animate = function () {
     })
   }
   recyclePlanes(elements);
-  tunnelEnd += descentSpeed;
   light2.position.y += camera.position.y + 450;
 
 	renderer.render( scene, camera );
